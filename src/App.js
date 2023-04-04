43ceec889738d3b267eaee6960f7775b718f9304
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-04 19:31:55 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Login } from "simple-component-library-sat";
-import Carousel1 from "./caurousel/caurosel";
+import { Login ,Carousel1} from "simple-component-library-sat";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import  s1 from './images/s1.jpg'
+import  s2 from './images/s2.jpg';
+import  s3 from './images/s3.jpg';
+import  s4 from './images/s4.jpg';
 const MyApp = () => {
   return (
     <>
     <div >
-        <Carousel1 ></Carousel1>
+        < Carousel1/>
       </div>
   
       <div>
